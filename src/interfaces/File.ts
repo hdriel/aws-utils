f14adee49dec1cbf78b3a6676940f6ea87cd8fa6
Hadriel Benjo <hdriel@gmail.com>
2025-10-03 16:33:51 +0300

    update s3-bucket util

--- src/interfaces/File.ts
+++ src/interfaces/File.ts
@@ -12,23 +12,3 @@ export interface File {
 export interface FILES3_METADATA extends File {
     directory: string;
 }
-
-export interface FileS3 {
-    fieldname: string;
-    originalname: string;
-    encoding: string;
-    mimetype: string;
-    size: number;
-    bucket: string;
-    key: string;
-    acl: string;
-    contentType: string;
-    contentDisposition: string | null;
-    contentEncoding: string | null;
-    storageClass: string;
-    serverSideEncryption: string | null;
-    metadata: string;
-    location: string;
-    etag: string;
-    versionId: string | undefined;
-}
