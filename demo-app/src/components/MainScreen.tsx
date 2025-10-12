69bca49f0dc95b7e83ecf136797fe588effe1485
Hadriel Benjo <hdriel@gmail.com>
2025-10-12 20:01:12 +0300

    update s3 service

--- demo-app/src/components/MainScreen.tsx
+++ demo-app/src/components/MainScreen.tsx
@@ -89,7 +89,7 @@ const MainScreen: React.FC<MainScreenProps> = ({ bucketName, bucketAccess, onLog
                             onFolderSelect={setCurrentPath}
                             onRefresh={handleRefresh}
                             refreshTrigger={refreshTrigger}
-                            localstack={!localstack}
+                            localstack={localstack}
                         />
                     </Panel>
                     <PanelResizeHandle />
